35dd18d4de39f7639eb8cc70db6877de10fdf5c8
Abhishek <abhisheklinkd5@gmail.com>
2023-10-01 22:37:27 +0530

    stage 2 prepare based model added

--- DL-PROJECT/Chicken-diseases-classification/main.py
+++ DL-PROJECT/Chicken-diseases-classification/main.py
@@ -1,11 +1,24 @@
 from chicken_diseases_prediction import logger
 from chicken_diseases_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from chicken_diseases_prediction.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
    data_ingestion = DataIngestionTrainingPipeline()
    data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
