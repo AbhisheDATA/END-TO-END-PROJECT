3ce61017fd322eebdf7fb4ffd25ea1e8fa3d6a7f
Abhishek <abhisheklinkd5@gmail.com>
2023-10-09 11:21:22 +0530

    training added

--- DL-PROJECT/Chicken-diseases-classification/main.py
+++ DL-PROJECT/Chicken-diseases-classification/main.py
@@ -1,6 +1,8 @@
 from chicken_diseases_prediction import logger
 from chicken_diseases_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from chicken_diseases_prediction.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from chicken_diseases_prediction.pipeline.stage_03_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -19,6 +21,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
